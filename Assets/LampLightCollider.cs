4641329ec57774366c8b5d99c83f1a1b4c41faa4
Nilvan <jean_v45@hotmail.com>
2021-01-30 01:47:03 +0100

    Changing rendering to LWRP
    Change rending mode to enable light effect as shadows

---
+++ Assets/LampLightCollider.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Experimental.Rendering;
+public class LampLightCollider : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    void OnTriggerEnter2D(Collider2D other){
+        
+        if(other.gameObject.CompareTag("DeadByLight")){
+            Color target = other.gameObject.GetComponent<SpriteRenderer>().color;
+        
+           // other.gameObject.GetComponent<SpriteRenderer>().color = new Color (target.r,target.g,target.b,0.20f);
+           // other.gameObject.GetComponent<ShadowCaster2D>().castsShadows = false;
+        }
+        
+
+    }
+    void OnTriggerExit2D(Collider2D other){
+        
+        if(other.gameObject.CompareTag("DeadByLight")){
+            Color target = other.gameObject.GetComponent<SpriteRenderer>().color;
+            target.a = 1f;
+            //other.gameObject.GetComponent<SpriteRenderer>().color = target;
+        }
+
+    }
+}
